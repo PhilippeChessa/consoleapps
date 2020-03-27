337c0865b2623ce1f02ca6b86326a70a37acfe9d
Philiipe.Cheesa <p.chessa@cegi.fr>
2020-03-27 15:35:45 +0100

    1er archivage

---
+++ AzureFiles/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.Azure;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.Storage.File;
+
+namespace AzureFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Parse the connection string and return a reference to the storage account.
+            // CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=personal5772156649;AccountKey=GPV82gR7e7+1woWq0MwIlVU6zrNg1OCE+9/+cY1vCWHE6gfXzzGvscGNxnTerRiiXToiu+Du0yGLcq0MF7kLRg==;EndpointSuffix=core.windows.net");
+
+            // Create a CloudFileClient object for credentialed access to Azure Files.
+            CloudFileClient fileClient = storageAccount.CreateCloudFileClient();
+
+            // Get a reference to the file share we created previously.
+            CloudFileShare share = fileClient.GetShareReference("genrestxt");
+
+            // Ensure that the share exists.
+            if (share.Exists())
+            {
+                // Get a reference to the root directory for the share.
+                CloudFileDirectory rootDir = share.GetRootDirectoryReference();
+
+                // Get a reference to the directory we created previously.
+                CloudFileDirectory sampleDir = rootDir.GetDirectoryReference("CustomLogs");
+
+                // Ensure that the directory exists.
+                if (sampleDir.Exists())
+                {
+                    // Get a reference to the file we created previously.
+                    CloudFile file = sampleDir.GetFileReference("Log1.txt");
+
+                    // Ensure that the file exists.
+                    if (file.Exists())
+                    {
+                        // Write the contents of the file to the console window.
+                        Console.WriteLine(file.DownloadTextAsync().Result);
+                    }
+                }
+            }
+
+        }
+    }
+}
